1649a81be7c1b4eb3c5207c8705c0b8d7353860a
Santiago006 <101900493+Santiago006@users.noreply.github.com>
2022-03-29 19:20:40 +0200

    Update Ejemplo.java

--- MisProyectosJava/EjemploJava2022/src/main/java/Ejemplo.java
+++ MisProyectosJava/EjemploJava2022/src/main/java/Ejemplo.java
@@ -9,7 +9,7 @@
  */
 public class Ejemplo {
     public static void main(String[] args) {
-    System.out.print("Hola Santiago");
+    System.out.print("Hola Santiago, me alegra verte");
     System.out.print("Esto es nuevo");
             }
 }
